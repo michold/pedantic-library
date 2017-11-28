3aa7bc3071b5a623e56a6fe0f6aef0478bac8aa0
Michał Wielich <poiu.m.w@gmail.com>
2017-11-28 20:12:31 +0100

    list directories

---
+++ main.rb
@@ -0,0 +1,29 @@
+DEFAULT_LOCATION = '/Users/poiu/Desktop/'
+CWD = ARGV[0] || DEFAULT_LOCATION
+
+def main
+  Dir.chdir CWD
+  Dir.entries('./').each do |dir|
+    next if skip_dir?(dir)
+    puts dir
+  end
+end
+
+def skip_dir?(dir)
+  recursive?(dir) || file?(dir) || !has_music?(dir)
+end
+
+def recursive?(dir)
+  dir == '.' || dir == '..'
+end
+
+def file?(dir)
+  File.file? dir
+end
+
+def has_music?(dir)
+  # Dir.glob("**/*")
+  # binding.pry
+end
+
+main
