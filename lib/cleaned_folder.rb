0d61407631a4fba80404fe7d1c7b6d1852743278
Michał Wielich <poiu.m.w@gmail.com>
2017-11-29 19:58:26 +0100

    export tags retrieval to separate class

--- lib/cleaned_folder.rb
+++ lib/cleaned_folder.rb
@@ -8,10 +8,7 @@ class CleanedFolder
   def update!
     # assumes 1 folder = 1 album
     # TODO: handle multiple albums, not sure how though :<
-    scan_files_tags
-
-    artist = artists.uniq.length == 1 && artists[0]
-    album = albums.uniq.length == 1 && albums[0]
+    find_tags
 
     return unless artist && album
 
@@ -19,42 +16,24 @@ class CleanedFolder
     puts artist
     puts '.....'
     puts album
+    # puts files
+    puts folders
     puts '.....'
   end
 
   private
 
-  attr_reader :folder_name, :artists, :albums
+  attr_reader :folder_name, :artist, :album
 
-  def scan_files_tags
-    mp3_files.each do |file_path|
-      ID3Tag.read(File.open(file_path)) do |tag|
-        artists << find_artist(tag)
-        albums << find_album(tag)
-      end
-    end
+  def find_tags
+    tags = FileTags.new(folder_name)
+
+    @artist = tags.artists.uniq.length == 1 && tags.artists[0]
+    @album = tags.albums.uniq.length == 1 && tags.albums[0]
   end
 
-  def find_artist(tag)
-    begin
-      tag.artist
-    rescue ID3Tag::Tag::MultipleFrameError
-      last_hope(tag, :TIT2)
-    end
-  end
-
-  def find_album(tag)
-    begin
-      tag.album
-    rescue ID3Tag::Tag::MultipleFrameError
-      last_hope(tag, :TIT2)
-    end
-  end
-
-  def last_hope(tag, id)
-    frames = tag.all_frames_by_id(id)
-    results = frames.map(&:content).uniq
-    results.length == 1 ? results[0] : (raise D3Tag::Tag::MultipleFrameError)
+  def folders
+    @_folders ||= Dir.glob("#{bash_escape(folder_name)}/**/*/")
   end
 
   def files
@@ -63,11 +42,5 @@ class CleanedFolder
 
   def bash_escape(string)
     string.gsub(/[\\\{\}\[\]\*\?]/) { |symbol| "\\#{symbol}" }
-  end
-
-  def mp3_files
-    files.find_all do |file|
-      file.end_with? '.mp3'
-    end
   end
 end
---
+++ lib/file_tags.rb
@@ -0,0 +1,59 @@
+class FileTags
+  def initialize(folder_name)
+    @folder_name = folder_name
+    @artists = []
+    @albums = []
+    scan_files_tags
+  end
+
+  attr_reader :artists, :albums
+
+  private
+
+  attr_reader :folder_name
+
+  def scan_files_tags
+    mp3_files.each do |file_path|
+      ID3Tag.read(File.open(file_path)) do |tag|
+        artists << find_artist(tag)
+        albums << find_album(tag)
+      end
+    end
+  end
+
+  def find_artist(tag)
+    begin
+      tag.artist
+    rescue ID3Tag::Tag::MultipleFrameError
+      last_hope(tag, :TIT2)
+    end
+  end
+
+  def find_album(tag)
+    begin
+      tag.album
+    rescue ID3Tag::Tag::MultipleFrameError
+      last_hope(tag, :TIT2)
+    end
+  end
+
+  def last_hope(tag, id)
+    frames = tag.all_frames_by_id(id)
+    results = frames.map(&:content).uniq
+    results.length == 1 ? results[0] : (raise D3Tag::Tag::MultipleFrameError)
+  end
+
+  def files
+    @_files ||= Dir.glob("#{bash_escape(folder_name)}/**/*")
+  end
+
+  def bash_escape(string)
+    string.gsub(/[\\\{\}\[\]\*\?]/) { |symbol| "\\#{symbol}" }
+  end
+
+  def mp3_files
+    files.find_all do |file|
+      file.end_with? '.mp3'
+    end
+  end
+end
