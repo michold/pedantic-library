fc5f5c4ddc4e04858a902e20d27585de72b1c2dc
Michał Wielich <poiu.m.w@gmail.com>
2017-11-29 19:49:44 +0100

    refactor to smaller methods

--- lib/cleaned_folder.rb
+++ lib/cleaned_folder.rb
@@ -8,15 +8,7 @@ class CleanedFolder
   def update!
     # assumes 1 folder = 1 album
     # TODO: handle multiple albums, not sure how though :<
-    # puts folder_name
-    # puts "........."
-    mp3_files.each do |file_path|
-      puts file_path
-      ID3Tag.read(File.open(file_path)) do |tag|
-        artists << find_artist(tag)
-        albums << find_album(tag)
-      end
-    end
+    scan_files_tags
 
     artist = artists.uniq.length == 1 && artists[0]
     album = albums.uniq.length == 1 && albums[0]
@@ -28,37 +20,45 @@ class CleanedFolder
     puts '.....'
     puts album
     puts '.....'
-
-    # puts files
-    # puts "........."
   end
 
   private
 
   attr_reader :folder_name, :artists, :albums
 
+  def scan_files_tags
+    mp3_files.each do |file_path|
+      ID3Tag.read(File.open(file_path)) do |tag|
+        artists << find_artist(tag)
+        albums << find_album(tag)
+      end
+    end
+  end
+
   def find_artist(tag)
     begin
       tag.artist
-    rescue ID3Tag::Tag::MultipleFrameError => e
-      artist = last_hope(tag, :TIT2)
-      artist || (raise e)
+    rescue ID3Tag::Tag::MultipleFrameError
+      last_hope(tag, :TIT2)
     end
   end
 
   def find_album(tag)
     begin
       tag.album
-    rescue ID3Tag::Tag::MultipleFrameError => e
-      album = last_hope(tag, :TIT2)
-      album || (raise e)
+    rescue ID3Tag::Tag::MultipleFrameError
+      last_hope(tag, :TIT2)
     end
   end
 
   def last_hope(tag, id)
     frames = tag.all_frames_by_id(id)
     results = frames.map(&:content).uniq
-    results.length == 1 ? results[0] : nil
+    results.length == 1 ? results[0] : (raise D3Tag::Tag::MultipleFrameError)
+  end
+
+  def files
+    @_files ||= Dir.glob("#{bash_escape(folder_name)}/**/*")
   end
 
   def bash_escape(string)
@@ -66,7 +66,7 @@ class CleanedFolder
   end
 
   def mp3_files
-    Dir.glob("#{bash_escape(folder_name)}/**/*").find_all do |file|
+    files.find_all do |file|
       file.end_with? '.mp3'
     end
   end
