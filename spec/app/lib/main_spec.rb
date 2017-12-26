ad20e545e8d431311cb09ad0202ae849ac07f6ee
Michał Wielich <poiu.m.w@gmail.com>
2017-12-26 14:58:04 +0100

    add a test for main

---
+++ spec/app/lib/main_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+RSpec.describe Main do
+  describe 'searched path' do
+    before do
+      FoldersWithMusic.any_instance.stubs(names: [])
+      CleanedFolder.any_instance.stubs(:update!)
+    end
+
+    context 'with given path' do
+      let(:tested_folder) { 'test/fol_der' }
+
+      it 'searches given path' do
+        Dir.expects(:chdir).with(tested_folder)
+
+        Main.new(tested_folder).process
+      end
+    end
+
+    context 'without given path' do
+      it "searches user's desktop" do
+        Dir.expects(:chdir).with("/Users/#{ENV['USER']}/Desktop")
+
+        Main.new(nil).process
+      end
+    end
+  end
+end
